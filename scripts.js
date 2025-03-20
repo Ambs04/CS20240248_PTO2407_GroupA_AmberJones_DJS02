6f8cbaa01d91eafef1035e4b2527a84be13e22f2
Ambs04 <amberleejones2007@gmail.com>
2025-03-20 21:06:07 +0200

    first condition checks the dividend is divided by zero

--- scripts.js
+++ scripts.js
@@ -5,5 +5,12 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  //Divide normally with two numbers above zero
   result.innerText = Math.floor(dividend / divider);
+
+  if (dividend / divider === Infinity) {
+    result.innerText = `Division not performed. Invalid number provided. Try again.`;
+    throw new Error(`Dividend cannot be divided by zero.`);
+  }
 });
